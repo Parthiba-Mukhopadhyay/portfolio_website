c5150f0162d5d13158a11983598eafb8d6498c74
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-11-05 14:00:42 +0530

    updates

--- portfolio_website/components/About.jsx
+++ portfolio_website/components/About.jsx
@@ -3,6 +3,15 @@ import React from "react";
 import Image from "next/image";
 import Typed from "react-typed";
 import myavatar1 from "../assets/myavatar1.png";
+import instagram from "../assets/icons/instagram.svg";
+import linkedin from "../assets/icons/linkedin.svg";
+import X from "../assets/icons/x.svg";
+import github from "../assets/icons/github.svg";
+import facebook from "../assets/icons/facebook.svg";
+import medium from "../assets/icons/medium.svg";
+import leetcode from "../assets/icons/leetcode.svg";
+import hackerrank from "../assets/icons/hackerrank.svg";
+import stackoverflow from "../assets/icons/stackoverflow.svg";
 import Link from "next/link";
 const About = () => {
   return (
@@ -29,42 +38,100 @@ const About = () => {
                 loop
               />
             </div>
-            <p className="p-8 text-white mr-4">
-              I'm a pre-final year B.Tech student
-              majoring in Computer Science Engineering at VIT Chennai. I excel
-              in UI design and full-stack development. My expertise lies in AI
-              and ML, with a track record of crafting effective ML models for
-              various projects. 
-              <br></br><br></br> Let's connect and achieve great results
-              together!
+            <p className="px-8 pb-4 text-white mr-4">
+              I'm a pre-final year B.Tech student majoring in Computer Science
+              Engineering at VIT Chennai. I excel in UI design and full-stack
+              development. My expertise lies in AI and ML, with a track record
+              of crafting effective ML models for various projects.
             </p>
+            <p className="px-8 pb-4 font-semibold text-lg bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent mr-4"> Let's connect and achieve great results together!</p>
             <div>
-                <div className="pl-7 pb-3">
-                    <ul className="flex gap-3">
-                        <li>Instagram</li>
-                        <li>LinkedIn</li>
-                        <li>Twitter</li>
-                        <li>GitHub</li>
-                        <li>Facebook</li>
-                        <li>Medium</li>
-
-                    </ul>
-                </div>
+              <div className="pl-7 pb-3">
+                <ul className="flex gap-4">
+                  <li>
+                    <Image
+                      src={instagram}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="instagram"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={linkedin}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="linkedin"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={X}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="X"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={github}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="github"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={facebook}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="facebook"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={medium}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="medium"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={stackoverflow}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="stackoverflow"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={leetcode}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="leetcode"
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      src={hackerrank}
+                      className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                      alt="hackerrank"
+                    />
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="p-4">
-                <div>
-                    <p className="text-3xl font-semibold">EXPERIENCE</p>
-                </div>
-                <div className="p-3">
-                    <p className="text-xl bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent">Aug'23 - Oct'23</p>
-                    <p>HMT Architects</p>
-                    <p>Web Developer Intern</p>
-                </div>
-                <div className="p-3">
-                    <p className="text-xl bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent">Sep'23 - Present</p>
-                    <p>Alpha Code Labs</p>
-                    <p>Full stack Developer Intern</p>
-                </div>
+              <div>
+                <p className="text-3xl font-semibold">EXPERIENCE</p>
+              </div>
+              <div className="p-3">
+                <p className="text-xl bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+                  Aug'23 - Oct'23
+                </p>
+                <p>HMT Architects</p>
+                <p>Web Developer Intern</p>
+              </div>
+              <div className="p-3">
+                <p className="text-xl bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+                  Sep'23 - Present
+                </p>
+                <p>Alpha Code Labs</p>
+                <p>Full stack Developer Intern</p>
+              </div>
             </div>
           </div>
         </div>
--- portfolio_website/components/Skills.jsx
+++ portfolio_website/components/Skills.jsx
@@ -1,9 +1,23 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Typed from "react-typed";
-import myavatar1 from "../assets/myavatar1.png";
-import Link from "next/link";
+import bootstrap from "../assets/icons/bootstrap.svg";
+import c from "../assets/icons/c.svg";
+import cpp from "../assets/icons/c++.svg";
+import css from "../assets/icons/css.svg";
+import expressjs from "../assets/icons/express.svg";
+import figma from "../assets/icons/figma.svg";
+import git from "../assets/icons/git.svg";
+import html from "../assets/icons/html.svg";
+import java from "../assets/icons/java.svg";
+import javascript from "../assets/icons/javascript.svg";
+import linux from "../assets/icons/linux.svg";
+import mongodb from "../assets/icons/mongodb.svg";
+import mysql from "../assets/icons/mysql.svg";
+import nextjs from "../assets/icons/nextjs.svg";
+import nodejs from "../assets/icons/nodejs.svg";
+import reactjs from "../assets/icons/reactjs.svg";
+import tailwind from "../assets/icons/tailwind.svg";
 
 const Skills = () => {
   return (
@@ -11,9 +25,101 @@ const Skills = () => {
       <div className="lg:m-22 md:pl-14 m-8 pb-16">
         <div className="text-center">
           <p className="text-7xl p-4">Skills</p>
+          <p className="text-2xl p-4">
+            Check out some technologies I worked with!
+          </p>
         </div>
         <div>
+          <div className="pl-7 pb-3">
+            <ul className="flex gap-4">
+              <li>
+                <Image
+                  src={html}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="html"
+                />
+              </li>
+              <li>
+                <Image
+                  src={css}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="css"
+                />
+              </li>
+              <li>
+                <Image
+                  src={javascript}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="javascript"
+                />
+              </li>
+              <li>
+                <Image
+                  src={bootstrap}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="bootstrap"
+                />
+              </li>
+              <li>
+                <Image
+                  src={tailwind}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="tailwind"
+                />
+              </li>
+              <li>
+                <Image
+                  src={nodejs}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="nodejs"
+                />
+              </li>
+              <li>
+                <Image
+                  src={expressjs}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="expressjs"
+                />
+              </li>
+              <li>
+                <Image
+                  src={mysql}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="mysql"
+                />
+              </li>
+              <li>
+                <Image
+                  src={mongodb}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="mongodb"
+                />
+              </li>
+              <li>
+                <Image
+                  src={reactjs}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="reactjs"
+                />
+              </li>
+              <li>
+                <Image
+                  src={nextjs}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="nextjs"
+                />
+              </li>
+              <li>
+                <Image
+                  src={mongodb}
+                  className="h-10 w-10 hover:animate-bounce rounded-lg shadow-xl dark:shadow-gray-800"
+                  alt="mongodb"
+                />
+              </li>
+            </ul>
+          </div>
         </div>
+       
       </div>
     </div>
   );
