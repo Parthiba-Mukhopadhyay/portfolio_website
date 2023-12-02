41bc05ad10c185aaa20cb0a4a61efa75b0086e92
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-12-02 19:11:47 +0530

    updates

--- portfolio_website/components/seeproject.jsx
+++ portfolio_website/components/seeproject.jsx
@@ -11,7 +11,10 @@ import mediaplayer from "../assets/projects/mediaplayer.png";
 import nlp from "../assets/projects/nlp.png";
 import github from "../assets/icons/github.svg";
 import figma from "../assets/icons/figma.svg";
-import gif from "../assets/GIF/tenor.gif"
+import design_logo from "../assets/design_logo.png";
+import development_logo from "../assets/development_logo.png";
+import AIML_logo from "../assets/AIML_logo.png";
+
 import Link from "next/link";
 
 const ProjectCard = ({ pic, name, summary, tech, project, icon }) => {
@@ -56,26 +59,36 @@ const Seeproject = () => {
         <p className="text-7xl p-4">My projects</p>
       </div>
       <div>
-        <p className="text-4xl py-8 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+        <p className="text-2xl pb-8 pt-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
           Check my projects out!
         </p>
       </div>
 
       <div className="flex flex-row gap-20 items-center justify-center">
-        <div className="flex flex-row cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-48 w-40">
-          <p>Design</p>
-        </div>
-        <div className="flex flex-row cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-48 w-40">
-          <p>Development</p>
-        </div>
-        <div className="flex flex-row cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-48 w-40">
-          <p>AI/ML</p>
-        </div>
-        <Image
-            src={gif}
-            className="h-40 mr-28 w-40 rounded-lg"
-            alt="github"
+        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+          <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> Design</p>
+          <Image
+            src={design_logo}
+            className="h-30 w-30 rounded-lg"
+            alt="/"
           />
+        </div>
+        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+        <p className="text-3xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> Development</p>
+          <Image
+            src={development_logo}
+            className="h-44 w-44 rounded-lg"
+            alt="/"
+          />
+        </div>
+        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+        <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> AI/ML</p>
+          <Image
+            src={AIML_logo}
+            className="h-44 w-44 rounded-lg"
+            alt="/"
+          />
+        </div>
       </div>
 
 
