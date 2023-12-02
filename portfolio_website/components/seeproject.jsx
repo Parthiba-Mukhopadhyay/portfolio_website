e52527801e08358e8f248807e4948479349928bb
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-12-02 20:52:34 +0530

    updates

--- portfolio_website/components/seeproject.jsx
+++ portfolio_website/components/seeproject.jsx
@@ -65,39 +65,44 @@ const Seeproject = () => {
       </div>
 
       <div className="flex flex-row gap-20 items-center justify-center">
-        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
-          <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> Design</p>
-          <Image
-            src={design_logo}
-            className="h-30 w-30 rounded-lg"
-            alt="/"
-          />
-        </div>
-        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
-        <p className="text-3xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> Development</p>
-          <Image
-            src={development_logo}
-            className="h-44 w-44 rounded-lg"
-            alt="/"
-          />
-        </div>
-        <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
-        <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold"> AI/ML</p>
-          <Image
-            src={AIML_logo}
-            className="h-44 w-44 rounded-lg"
-            alt="/"
-          />
-        </div>
+        <Link href="/Design">
+          <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+            <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+              {" "}
+              Design
+            </p>
+            <Image src={design_logo} className="h-30 w-30 rounded-lg" alt="/" />
+          </div>
+        </Link>
+        <Link href="/Development">
+          <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+            <p className="text-3xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+              {" "}
+              Development
+            </p>
+            <Image
+              src={development_logo}
+              className="h-44 w-44 rounded-lg"
+              alt="/"
+            />
+          </div>
+        </Link>
+        <Link href="/Aiml">
+          <div className="flex flex-col cursor-pointer hover:scale-110 ease-in-out duration-300 items-center rounded-xl border-2 border-yellow-500 shadow-md dark:shadow-orange-400 h-60 w-56">
+            <p className="text-4xl p-4 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
+              {" "}
+              AI/ML
+            </p>
+            <Image src={AIML_logo} className="h-44 w-44 rounded-lg" alt="/" />
+          </div>
+        </Link>
       </div>
-
 
       <div>
         <p className="text-4xl pt-8 bg-gradient-to-br from-yellow-300 to-orange-500 bg-clip-text text-transparent font-semibold">
           Design and Development
         </p>
       </div>
-
 
       <div className="grid grid-cols-3 gap-x-20 gap-y-4">
         <ProjectCard
